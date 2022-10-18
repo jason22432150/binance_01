d9d08efcae1668cb32c8df25a4f184b5b01919f1
Jhen <nmsl22432150@gmail.com>
2022-10-19 02:14:50 +0800

    save

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,14 +1,28 @@
 <template>
   <q-header> </q-header>
   <div class="pagination-1">
-    <div class="pagination-1-outside-container">
-      <div class="pagination-1-inside-container-1">
-        <div class="title">在幣安購買、交易和持有超過 350 種加密貨幣</div>
-        <button>透過電子郵件或電話註冊</button>
-        <div></div>
+    <div :style="{ padding: '0 24px 0 24px' }">
+      <div :style="{ padding: '80px 0 0 0' }">
+        <div :style="{ width: '560px', margin: '0 106px 0 0' }">
+          <h1 class="title">在幣安購買、交易和持有超過 350 種加密貨幣</h1>
+          <div >
+            <a href="test" class="btn-email">透過電子郵件或電話註冊</a>
+            <div
+              :style="{
+                display: 'flex',
+                'align-items': 'center',
+                margin: '16px 0 16px 0',
+                width: '384px',
+              }"
+            >
+              <div class="line-style" />
+              <div :style="{ margin: '0 48px 0 48px' }">或</div>
+              <div class="line-style" />
+            </div>
+            <div><a href="" :style="{ 'box-sizing': 'border-box' }"></a></div>
+          </div>
+        </div>
       </div>
-      <div class="pagination-1-inside-container-2"></div>
-      <div class="pagination-1-inside-container-3"></div>
     </div>
   </div>
   <div class="pagination-2">
@@ -16,11 +30,7 @@
     <div class="test">123321132</div>
     <div class="test">123321132</div>
     <div class="test">123321132</div>
-    <div class="test">123321132</div>
-    <div class="test">123321132</div>
   </div>
-
-  <!-- <router-view></router-view> -->
 </template>
 
 <script lang="ts">
@@ -41,9 +51,37 @@ export default defineComponent({
   height: 100vh;
   background-color: aqua;
 }
-.title{
+.title {
   font-size: 40px;
   font-weight: 600;
   line-height: 48px;
+  margin: 0;
+}
+.pagination-1 {
+  padding: 80px 0 0 0;
+}
+.btn-email {
+  border-radius: 4px;
+  background-color: #fcd535;
+  width: 100%;
+  min-height: 24px;
+  color: #181a20;
+  text-align: center;
+  word-break: keep-all;
+  min-height: 24px;
+  min-width: 80px;
+  line-height: 24px;
+  padding: 12px 24px 12px 24px;
+  font-weight: 500;
+  font-size: 16px;
+  justify-content: center;
+  align-items: center;
+  /* box-sizing: border-box; */
+}
+.line-style {
+  height: 1px;
+  flex: 1;
+  background-color: #eaecef;
+  box-sizing: border-box;
 }
 </style>
